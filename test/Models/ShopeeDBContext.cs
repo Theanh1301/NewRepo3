812bba448aaa431e95cbb1da464fc793001738ae
haianh <haianh@DESKTOP-0P0S8VJ>
2023-02-23 14:02:37 +0700

    Project

---
+++ test/Models/ShopeeDBContext.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace test.Models
+{
+    public partial class ShopeeDBContext : DbContext
+    {
+        public ShopeeDBContext()
+        {
+        }
+
+        public ShopeeDBContext(DbContextOptions<ShopeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<ChatHistory> ChatHistories { get; set; } = null!;
+        public virtual DbSet<FlashSale> FlashSales { get; set; } = null!;
+        public virtual DbSet<Order> Orders { get; set; } = null!;
+        public virtual DbSet<OrderProduct> OrderProducts { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-0P0S8VJ\\MAYAO;database=ShopeeDB;Integrated security=true;trustServerCertificate=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            });
+
+            modelBuilder.Entity<ChatHistory>(entity =>
+            {
+                entity.HasKey(e => e.ChatId);
+
+                entity.ToTable("ChatHistory");
+
+                entity.Property(e => e.ChatId).HasColumnName("ChatID");
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.Property(e => e.UserId1).HasColumnName("UserID1");
+
+                entity.Property(e => e.UserId2).HasColumnName("UserID2");
+
+                entity.HasOne(d => d.UserId1Navigation)
+                    .WithMany(p => p.ChatHistories)
+                    .HasForeignKey(d => d.UserId1)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ChatHistory_Users");
+            });
+
+            modelBuilder.Entity<FlashSale>(entity =>
+            {
+                entity.HasKey(e => e.Fid);
+
+                entity.ToTable("FlashSale");
+
+                entity.Property(e => e.Fid).HasColumnName("FID");
+
+                entity.Property(e => e.DateEnd).HasColumnType("date");
+
+                entity.Property(e => e.DateStart).HasColumnType("date");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.FlashSales)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_FlashSale_Product");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.Date).HasColumnType("date");
+
+                entity.Property(e => e.Total).HasColumnType("money");
+            });
+
+            modelBuilder.Entity<OrderProduct>(entity =>
+            {
+                entity.ToTable("OrderProduct");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Oid).HasColumnName("OID");
+
+                entity.Property(e => e.Pid).HasColumnName("PID");
+
+                entity.HasOne(d => d.OidNavigation)
+                    .WithMany(p => p.OrderProducts)
+                    .HasForeignKey(d => d.Oid)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OrderProduct_Orders");
+
+                entity.HasOne(d => d.PidNavigation)
+                    .WithMany(p => p.OrderProducts)
+                    .HasForeignKey(d => d.Pid)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OrderProduct_Product");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.SaleId).HasColumnName("SaleID");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Product_Categories");
+
+                entity.HasOne(d => d.Sale)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.SaleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Product_Users");
+            });
+
+            modelBuilder.Entity<ShoppingCart>(entity =>
+            {
+                entity.HasKey(e => e.Sid);
+
+                entity.ToTable("ShoppingCart");
+
+                entity.Property(e => e.Sid).HasColumnName("SID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.ShoppingCarts)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ShoppingCart_Product");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.ShoppingCarts)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ShoppingCart_Users");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

fbbb33f25d83739f6f66a60df6e25ea4c5e9e9a4
Nguyen Tuan <vnt28012000@gmail.com>
2023-02-27 14:44:20 +0700

    Valid Register

--- Shopee/Controllers/RegisterController.cs
+++ Shopee/Controllers/RegisterController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Shopee.Models;
+using System.Net.Mail;
 
 namespace Shopee.Controllers
 {
@@ -8,17 +9,26 @@ namespace Shopee.Controllers
     [ApiController]
     public class RegisterController : ControllerBase
     {
+        Validation validation = new Validation();
         ShopeeDBContext context = new ShopeeDBContext();
-
+        
         [HttpPost]
         [Route("register")]
-        public async Task<IActionResult> Register(string username, string password, string name,string phone, string email)
+        public async Task<IActionResult> Register(string username, string password, string email, string phone, string name)
         {
             var user = context.Users.SingleOrDefault(u => u.Username == username);
 
             if (user != null)
             {
                 return BadRequest("Username is exist!");
+            }
+            else if (!validation.IsEmail(email))
+            {
+                return BadRequest("Email is not valid!");
+            }
+            else if (!validation.IsNumber(phone))
+            {
+                return BadRequest("Phone must be digit!");
             }
             else
             {
@@ -43,7 +53,7 @@ namespace Shopee.Controllers
                 {
                     return BadRequest();
                 }
-            }   
+            }
         }
     }
 }

0df39166e0fc475774eb0c6be4b0b9f3fe0779c6
Thế Anh Nguyễn <anhnguyendz9294@gmail.com>
2023-02-28 16:30:15 +0700

    login

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Signin from './Signin';
-import Profile from './Profile';
 
 function App() {
   // const token = localStorage.getItem('accessToken');

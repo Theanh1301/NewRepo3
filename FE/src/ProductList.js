cd4863873c9d76ab75b3557d7c8b29af52ec58e3
namkinq <nguyenhongpf@gmail.com>
2023-03-16 16:34:40 +0700

    create pagination

--- FE/src/ProductList.js
+++ FE/src/ProductList.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import ReactPaginate from 'react-paginate';
+import './pagination.css';
 
 const VND = new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
+  style: 'currency',
+  currency: 'VND',
 });
 
 function ProductList() {
@@ -14,12 +16,15 @@ function ProductList() {
       .then((data) => setProducts(data));
   }, []);
 
-  return (
-    <div>
-      <h1>Product List</h1>
-      <table border="1" width="100%" >
-        <thead align="center">
-            <tr>
+  //table
+  function Items({ currentItems }) {
+    return (
+      <>
+        <div>
+          <h1>Product List</h1>
+          <table border="1" width="100%" >
+            <thead align="center">
+              <tr>
                 <th width="250">Name</th>
                 <th width="200">Image</th>
                 <th>Price</th>
@@ -28,25 +33,83 @@ function ProductList() {
                 <th>Manufacturer</th>
                 <th>Saler</th>
                 <th>Add to cart</th>
-            </tr>
-        </thead>
-        <tbody align="center">
-            {products.map((product) => (
-                <tr>
-                    <td><a href={`/Product/${product.productId}`}>{product.productName}</a></td>
-                    <td>{product.image}</td>
-                    <td>{VND.format(product.unitPrice)}</td>
-                    <td>{product.unitInStock}</td>
-                    <td>{product.categoryName}</td>
-                    <td>{product.manufacturer}</td>
-                    <td>{product.saler}</td>
-                    <td><button style={{backgroundColor:'pink', borderRadius: '8px'
-                    , border:'2px solid #000080', color: 'red', fontWeight:'bold' } }>Add</button></td>
+              </tr>
+            </thead>
+            <tbody align="center">
+              {currentItems && currentItems.map((item) => (
+                <tr key={item.productId}>
+                  <td><a href={`/Product/${item.productId}`}>{item.productName}</a></td>
+                  <td>{item.image}</td>
+                  <td>{VND.format(item.unitPrice)}</td>
+                  <td>{item.unitInStock}</td>
+                  <td>{item.categoryName}</td>
+                  <td>{item.manufacturer}</td>
+                  <td>{item.saler}</td>
+                  <td><button style={{
+                    backgroundColor: 'pink', borderRadius: '8px'
+                    , border: '2px solid #000080', color: 'red', fontWeight: 'bold'
+                  }}>Add</button></td>
                 </tr>
-            ))}
-        </tbody>
-      </table>
-      
+              ))}
+            </tbody>
+          </table>
+          <br />
+        </div>
+      </>
+    );
+  }
+
+  function PaginatedItems({ itemsPerPage }) {
+    const [itemOffset, setItemOffset] = useState(0);
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    const currentItems = products.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(products.length / itemsPerPage);
+
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage) % products.length;
+      console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+      );
+      setItemOffset(newOffset);
+    };
+
+    return (
+      <div >
+        <Items currentItems={currentItems} />
+        <div className="pagination">
+          <ReactPaginate
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={2}
+            pageCount={pageCount}
+            previousLabel="< previous"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakLabel="..."
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            renderOnZeroPageCount={null}
+          />
+        </div>
+
+      </div>
+    );
+  }
+
+
+  return (
+    <div>
+
+      <PaginatedItems itemsPerPage={3} />
+
     </div>
   );
 }
